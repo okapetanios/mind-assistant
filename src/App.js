54832c67732e8ee22d357b86bd89c1948b2764d7
davischandram <davischandram@gmail.com>
2020-03-05 19:28:14 -0700

    Added header, search components, and store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import {combineReducers, createStore} from "redux";
+import {Provider} from "react-redux";
+import SearchComponent from "./components/SearchComponent";
+import Home from "./Home";
+
+const state = {};
+const reducers = combineReducers({
+    reducer: state
+});
+
+const store = createStore(reducers);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Provider store={store}>
+          <Router>
+              <div>
+                  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                      <a className="navbar-brand" href="/">Mind-Assistant</a>
+
+                      <div className="collapse navbar-collapse">
+                          <ul className="navbar-nav mr-auto">
+                          </ul>
+                          <form className="form-inline my-2 my-lg-0">
+                              <input className="form-control mr-sm-2" type="search" placeholder="Search"
+                                     aria-label="Search"/>
+                              <Link to={"/search"}>
+                                  <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                              </Link>
+                          </form>
+                      </div>
+                  </nav>
+
+                  <Switch>
+                      <Route
+                          path={"/search"}
+                          render={(props) =>
+                              <SearchComponent
+                                  {...props}
+                              />
+                      }/>
+                      <Route path={"/"}>
+                          <Home/>
+                      </Route>
+                  </Switch>
+              </div>
+          </Router>
+      </Provider>
   );
 }
 
---
+++ src/components/SearchComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+import SearchDetailsComponent from "./SearchDetailsComponent";
+
+const SearchComponent = () =>
+    <div>
+        <SearchDetailsComponent/>
+    </div>;
+
+export default SearchComponent
