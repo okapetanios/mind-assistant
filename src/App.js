4d3439005ae148b76b5cbd38b52a565ae56dcf21
okapetanios <ericegan@live.com>
2020-03-09 16:51:01 -0700

    Search functionality with SearchComponent complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import {
     BrowserRouter as Router,
@@ -6,55 +6,91 @@ import {
     Route,
     Link
 } from "react-router-dom";
-import {combineReducers, createStore} from "redux";
-import {Provider} from "react-redux";
+import { combineReducers, createStore } from "redux";
+import { Provider } from "react-redux";
 import SearchComponent from "./components/SearchComponent";
 import Home from "./Home";
 
-const state = {};
-const reducers = combineReducers({
-    reducer: state
-});
 
-const store = createStore(reducers);
 
-function App() {
-  return (
-      <Provider store={store}>
-          <Router>
-              <div>
-                  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                      <a className="navbar-brand" href="/">Mind-Assistant</a>
 
-                      <div className="collapse navbar-collapse">
-                          <ul className="navbar-nav mr-auto">
-                          </ul>
-                          <form className="form-inline my-2 my-lg-0">
-                              <input className="form-control mr-sm-2" type="search" placeholder="Search"
-                                     aria-label="Search"/>
-                              <Link to={"/search"}>
-                                  <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                              </Link>
-                          </form>
-                      </div>
-                  </nav>
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            keyword: '',
+            jokes: []
+        }
+    }
 
-                  <Switch>
-                      <Route
-                          path={"/search"}
-                          render={(props) =>
-                              <SearchComponent
-                                  {...props}
-                              />
-                      }/>
-                      <Route path={"/"}>
-                          <Home/>
-                      </Route>
-                  </Switch>
-              </div>
-          </Router>
-      </Provider>
-  );
+
+    reducers = combineReducers({
+        reducer: this.state
+    });
+
+    store = createStore(this.reducers);
+
+    searchJokes = () => {
+       
+        fetch(`https://icanhazdadjoke.com/search?term=${this.state.keyword}`, {
+            headers: {
+                Accept: "application/json"
+            }
+        })
+            .then(response => response.json())
+            .then(this.renderJokes)
+    }
+
+    renderJokes = (search) => {
+        this.setState({
+            jokes: search.results
+        })
+    }
+
+    keyWordChange = event =>
+        this.setState({ keyword: event.target.value })
+
+    render() {
+        return (
+            <Provider store={this.store}>
+                <Router>
+                    <div>
+                        <nav className="navbar navbar-expand navbar-dark bg-dark">
+                            <a className="navbar-brand" href="/">Mind-Assistant</a>
+
+                            <div className="collapse navbar-collapse">
+                                <ul className="navbar-nav mr-auto">
+                                </ul>
+                                <form className="form-inline my-2 my-lg-0">
+                                    <input value={this.state.keyword} onChange={this.keyWordChange} className="form-control mr-sm-2" type="search" placeholder="Search"
+                                        aria-label="Search" />
+                                    <Link to={"/search"}>
+                                        <button onClick={this.searchJokes} className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                                    </Link>
+                                </form>
+                            </div>
+                        </nav>
+
+                        <Switch>
+                            <Route
+                                path={"/search"}
+                                render={(props) =>
+                                    <SearchComponent
+                                        {...props}
+                                        jokes={this.state.jokes}
+                                        keyword={this.state.keyword === '' ? 'AllJokes' : this.state.keyword}
+
+
+                                    />
+                                } />
+                            <Route path={"/"}>
+                                <Home />
+                            </Route>
+                        </Switch>
+                    </div>
+                </Router>
+            </Provider>
+        );
+    }
 }
-
 export default App;
--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,9 +1,55 @@
 import React from "react";
+import {
+
+    Link
+} from "react-router-dom";
 import SearchDetailsComponent from "./SearchDetailsComponent";
+import { connect } from 'redux'
 
-const SearchComponent = () =>
-    <div>
-        <SearchDetailsComponent/>
-    </div>;
 
+
+class SearchComponent extends React.Component {
+    // I need to provide a list of dad jokes to SearchDetailsComonent.
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+
+
+    }
+
+    componentDidMount() {
+
+        this.setState({
+            results: this.props.jokes
+        })
+
+    }
+
+    render() {
+        return (
+
+            <div>
+
+                <ul className="list-group">
+
+                    {
+                        this.props.jokes.map(joke =>
+                            <Link to={`/search/${this.props.keyword}/details/${joke.id}`}>
+                                <li className="list-group-item" key={joke.id} >{joke.joke}
+                                </li>
+                            </Link>
+                        )
+
+                    }
+
+                </ul>
+
+            </div>
+
+
+        )
+    }
+}
 export default SearchComponent
