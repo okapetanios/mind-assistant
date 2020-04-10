e41f6b65139a078f238d10740147bcceaf37142f
JHutnick <jhutnick10@gmail.com>
2020-04-10 10:36:43 -0400

    Added Updated Profile Components
    Updated routing to a public and private profile component.  Added 2 layouts for 2 profile components

--- src/components/ProfileComponent.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const ProfileComponent = ({profileId, history}) =>
-    <div className={"container-fluid"}>
-        <h1>User Profile: {profileId}</h1>
-    </div>;
-
-export default ProfileComponent
