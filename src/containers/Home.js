83bff9d258ac70ae7d9550568de3ff6794fe19ac
davischandram <davischandram@gmail.com>
2020-04-19 14:10:07 -0600

    Added user information to home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,23 +1,32 @@
 import React, { Component } from "react";
+import "./Home.css"
 import NoteListComponent from '../components/note/NoteListComponent'
 import FolderListComponent from '../components/folder/FolderListComponent'
 import {findCurrentUser, logoutUser} from "../actions/userActions";
 import {connect} from "react-redux";
 import userService from "../services/userService";
+import LabelListComponent from "../components/label/LabelListComponent";
 
 const UserService = new userService();
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     componentDidMount() {
-        this.props.findCurrentUser()
+        this.props.findCurrentUser();
     }
 
     render() {
         return (
             <div className="App">
+                <div className={"ma-welcome"}>
+                    <h1>Hello {this.props.user.username}!</h1>
+                </div>
                 <div className="row">
                     <div className="col-4">
-                        <FolderListComponent/>
+                        <LabelListComponent/>
                     </div>
                     <div className="col-6">
                         <NoteListComponent/>
