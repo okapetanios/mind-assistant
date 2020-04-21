a70ea889a7763046bc7851fb4a7b956f5a55c6d9
davischandram <davischandram@gmail.com>
2020-04-20 19:25:36 -0600

    Updated non-user profile

--- src/components/profile/PublicProfileComponent.js
+++ src/components/profile/PublicProfileComponent.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import '../../App.css';
 import {connect} from "react-redux";
-// import userService from "../../services/userService";
 import profileService from "../../services/profileService";
 import {findProfile} from "../../actions/profileActions";
 
-// const UserService = new userService();
 const ProfileService = new profileService();
 
 class PublicProfileComponent extends React.Component {
@@ -14,15 +12,18 @@ class PublicProfileComponent extends React.Component {
     };
 
     state = {
-        profile: {id: 1, user: {username: "Testing"}}
+        profile: {id: 1, user: {username: "Testing"}},
+        notes: [{id: 1, title: "Test", note: "Note"}]
     };
 
     componentDidUpdate(prevProps, prevState, snapshot) {
         if(this.state.profile.id === 1 && this.props.profile.id > 0){
             console.log(this.props.profile);
+            console.log(this.props.profile.user.notes);
             this.setState({
-                profile: this.props.profile
-            })
+                profile: this.props.profile,
+                notes: this.props.profile.user.notes.filter(note => note.status === "public")
+            });
         }
     }
 
@@ -33,26 +34,25 @@ class PublicProfileComponent extends React.Component {
                 <div className="media">
                     <img src={this.state.profile.picture} className="mr-3" alt=""/>
                         <div className="media-body">
-                            <h1>{this.state.profile.user.username}'s Profile</h1>
-                            <form>
-                                <div className="form-group">
-                                    <label htmlFor="editInputEmail">Username: "TBD USERS USERNAME"</label>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="editFirstName">First Name: "USER FIRSTNAME"</label>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="editLastName">Last Name: "USER LASTNAME"</label>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="formControlSelect">Role: "USER ROLE"</label>
-                                </div>
-                            </form>
+                            <h1>{this.state.profile.user.fname} {this.state.profile.user.lname}</h1>
+                            <h6>{this.state.profile.user.username}</h6>
+                            <br/>
+                            About:
+                            <p>{this.state.profile.bio}</p>
                         </div>
                 </div>
                 <br/>
-                <div >
-                    <h3>Saved Notes</h3>
+                <div className={"text-center"}>
+                    <h3>Saved Public Notes</h3>
+                    <ul className="list-group">
+                        {this.state.notes.map(note =>
+                            <li className="list-group-item"
+                                key={note.id}>
+                                <h6>{note.title}</h6>
+                                <p>{note.note}</p>
+                            </li>
+                        )}
+                    </ul>
                 </div>
             </div>
         )
