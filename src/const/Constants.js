42459478c8f41882cb4cc1638461a3802281c92b
davischandram <davischandram@gmail.com>
2020-04-21 12:21:56 -0600

    updated heroku backend

--- src/const/Constants.js
+++ src/const/Constants.js
@@ -1,3 +1,4 @@
 export const JOKE_URL = "https://icanhazdadjoke.com";
-export const HEROKU_URL = "https://infinite-badlands-23294.herokuapp.com";
+// export const HEROKU_URL = "https://infinite-badlands-23294.herokuapp.com";
 // export const HEROKU_URL = "http://localhost:8080";
+export const HEROKU_URL = "https://mind-assistant-api.herokuapp.com/";
