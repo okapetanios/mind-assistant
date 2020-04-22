0dfa660a405fa4260b7378a1139a20078e7daa0c
JHutnick <jhutnick10@gmail.com>
2020-04-22 00:01:18 -0400

    Added to registration

--- src/components/account/RegisterComponent.js
+++ src/components/account/RegisterComponent.js
@@ -5,43 +5,85 @@ import {connect} from "react-redux";
 
 const UserService = new userService();
 
-class RegisterComponent extends React.Component{
+class RegisterComponent extends React.Component {
     state = {
         username: "",
+        newFname: "",
+        newLname: "",
+        newRole: "",
+        occupation: "",
+        school: "",
         password: "",
         verifyPassword: ""
     };
 
     componentDidUpdate(prevProps, prevState, snapshot) {
-        if(this.props.user.id !== prevProps.user.id && this.props.user.id > 0 ){
+        if (this.props.user.id !== prevProps.user.id && this.props.user.id > 0) {
             this.props.history.push("/profile")
         }
     }
 
     updateUsername = (e) => {
         this.setState({
-            username: e.target.value
-        })
+                          username: e.target.value
+                      })
     };
 
     updatePassword = (e) => {
         this.setState({
-            password: e.target.value
-        })
+                          password: e.target.value
+                      })
     };
 
     updateVerify = (e) => {
         this.setState({
-            verifyPassword: e.target.value
-        })
+                          verifyPassword: e.target.value
+                      })
     };
 
-    navigate =() => {
+    updateFirstName = (e) => {
+        this.setState({
+                          newFname: e.target.value
+                      })
+    };
+    updateLastName = (e) => {
+        this.setState({
+                          newLname: e.target.value
+                      })
+    };
+
+    roleChange = (e) => {
+        this.setState({
+                          newRole: e.target.value
+                      })
+    };
+
+    updateSchool = (e) => {
+        this.setState({
+                          school: e.target.value
+                      })
+    };
+
+    updateOccupation = (e) => {
+        this.setState({
+                          occupation: e.target.value
+                      })
+    };
+
+    navigate = () => {
         this.props.history.push("/profile")
     }
 
     register = () => {
-        const user = {username: this.state.username, password: this.state.password};
+        const user = {
+            username: this.state.username,
+            fname: this.state.newFname,
+            lname: this.state.newLname,
+            role: this.state.newRole,
+            school: this.state.school,
+            occupation: this.state.occupation,
+            password: this.state.password
+        };
         this.props.createUser(user);
     };
 
@@ -66,6 +108,91 @@ class RegisterComponent extends React.Component{
                         />
                     </div>
                 </div>
+
+                <div className="form-group row">
+                    <label htmlFor="firstNameFld"
+                           className="col-sm-2 col-form-label">
+                        First Name
+                    </label>
+                    <div className="col-sm-10">
+                        <input type="text"
+                               className="form-control "
+                               id="firstNameFld"
+                               placeholder="alice"
+                               value={this.state.newFname}
+                               onChange={this.updateFirstName}
+                        />
+                    </div>
+                </div>
+
+                <div className="form-group row">
+                    <label htmlFor="lastNameFld"
+                           className="col-sm-2 col-form-label">
+                        Last Name
+                    </label>
+                    <div className="col-sm-10">
+                        <input type="text"
+                               className="form-control "
+                               id="lastNameFld"
+                               placeholder="alice"
+                               value={this.state.newLname}
+                               onChange={this.updateLastName}
+                        />
+                    </div>
+                </div>
+
+                <div className="form-group row">
+                    <label htmlFor="editRole"
+                           className="col-sm-2 col-form-label">
+                        Role
+                    </label>
+                    <div className="col-sm-10">
+                        <select className="form-control"
+                                id="editRole"
+                                placeholder={"student"}
+                                onChange={this.roleChange}
+                                value={this.state.newRole === "" ? this.props.user.role:this.state.newRole}
+                        >
+                            <option value={"student"}>Student</option>
+                            <option value={"general"}>Other</option>
+                        </select>
+                    </div>
+                </div>
+
+                {this.state.newRole === "student" &&
+                 <div className="form-group row">
+                     <label htmlFor="schoolFld"
+                            className="col-sm-2 col-form-label">
+                         School
+                     </label>
+                     <div className="col-sm-10">
+                         <input type="text"
+                                className="form-control "
+                                id="schoolFld"
+                                placeholder="University"
+                                value={this.state.school}
+                                onChange={this.updateSchool}
+                         />
+                     </div>
+                 </div>
+                }
+                {this.state.newRole !== "student" &&
+                 <div className="form-group row">
+                     <label htmlFor="occupationFld"
+                            className="col-sm-2 col-form-label">
+                         Occupation
+                     </label>
+                     <div className="col-sm-10">
+                         <input type="text"
+                                className="form-control "
+                                id="schoolFld"
+                                placeholder="Occupation"
+                                value={this.state.occupation}
+                                onChange={this.updateOccupation}
+                         />
+                     </div>
+                 </div>
+                }
 
                 {/*Password field with example*/}
                 <div className="form-group row">
@@ -106,7 +233,7 @@ class RegisterComponent extends React.Component{
                     <div className="col-sm-2 col-form-label"></div>
                     <div className="col-sm-10">
                         <button className="btn btn-primary btn-block"
-                           onClick={this.register}>
+                                onClick={this.register}>
                             Sign Up
                         </button>
                         <div>
@@ -127,7 +254,7 @@ const dispatchToPropertyMapper = (dispatch) => ({
     createUser: (user) => {
         UserService.createUser(user).then(createdUser => {
             dispatch(createUser(createdUser))
-            if(createdUser.id < 1){
+            if (createdUser.id < 1) {
                 alert("username exits, try another one")
             }
         })
