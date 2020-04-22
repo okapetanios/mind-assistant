f10aff0c72215ef8936585377edda8c156d253e4
davischandram <davischandram@gmail.com>
2020-04-22 01:36:59 -0600

    almost complete labels to new note

--- src/components/note/NewNoteComponent.js
+++ src/components/note/NewNoteComponent.js
@@ -7,7 +7,9 @@ class NewNoteComponent extends React.Component {
         newText: "Note Body",
         newStatus: "private",
         newLabel: "",
-        newLabels: [{id: 1, title: "blah1"}, {id: 2, title: "blah2"}, {id: 3, title: "blah3"}]
+        existingLabel: "none",
+        newLabels: [],
+        userLabels: [{id:1, title: "label1"}]
     };
 
     //TODO:
@@ -31,10 +33,35 @@ class NewNoteComponent extends React.Component {
         })
     };
 
-   labelChanged = (e) => {
+    labelChange = (e) => {
         this.setState({
             newLabel: e.target.value
         })
+    };
+
+    labelChanged = (e) => {
+        this.setState({
+            existingLabel: e.target.value
+        })
+    };
+
+    //need to create a label before add to note
+    createLabel = () => {
+        // this.props.createUserLabel({title: this.state.newLabel});
+        // this.props.createFolderLabel({title: this.state.newLabel});
+        this.setState(prevState => ({
+            newLabel: "",
+            newLabels: [...prevState.newLabels, this.props.label]
+        }));
+    };
+
+    //need to find the label before add
+    addLabel = (labelId) => {
+        // this.props.findLabel(labelId);
+        this.setState(prevState => ({
+            newLabel: "",
+            newLabels: [...prevState.newLabels, this.props.label]
+        }));
     };
 
     //TODO:
@@ -83,17 +110,48 @@ class NewNoteComponent extends React.Component {
                     </div>
                 </div>
                 <div className="form-group row">
-                    <label htmlFor="addLabel"
+                    <label htmlFor="addNewLabel"
                            className="col-sm-2 col-form-label">
-                        Label
+                        New Label
                     </label>
-                    <div className="col-sm-10">
+                    <div className="col-sm-9">
                         <input type="text"
                                className="form-control "
-                               id="addLabel"
+                               id="addNewLabel"
                                placeholder="New Label (if applicable)"
-                               onChange={this.labelChanged}
+                               onChange={this.labelChange}
+                               value={this.state.newLabel}
                         />
+                    </div>
+                    <div className={"col-1"}>
+                        <button className={"btn btn-primary"}
+                                onClick={() => this.createLabel()}>
+                            <i className="fas fa-plus"></i>
+                        </button>
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label htmlFor="addLabel"
+                           className="col-sm-2 col-form-label">
+                        Existing Label
+                    </label>
+                    <div className="col-sm-9">
+                        <select className="form-control"
+                                id="addLabel"
+                                placeholder={"none"}
+                                value={this.state.existingLabel}
+                                onChange={this.labelChanged}>
+                            <option value={"none"}>None</option>
+                            {this.state.userLabels.length > 0 && this.state.userLabels.map(label =>
+                                <option value={label.id}>{label.title}</option>
+                            )}
+                        </select>
+                    </div>
+                    <div className={"col-1"}>
+                        <button className={"btn btn-primary"}
+                                onClick={() => this.addLabel(this.state.existingLabel)}>
+                            <i className="fas fa-plus"></i>
+                        </button>
                     </div>
                 </div>
                 <div className="form-group row">
